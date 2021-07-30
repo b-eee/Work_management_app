f15336d552a7f806507e59a1e7986c99f28da782
ryusei tosa <ryusei0726@mac.com>
2021-07-30 16:23:34 +0900

    mapapiの削除

--- pages/map.vue
+++ pages/map.vue
@@ -1,26 +1,13 @@
 <template>
   <div>
-    <v-text-field
-      v-model="keyword"
-      label="search keyword"
-    ></v-text-field>
-    <div v-if="keyword" class="google-map">
+    <div class="google-map">
       <iframe
-        :src="
-          `https://www.google.com/maps/embed/v1/place?key=${mapApiKey}
-    &q=${keyword}`
-        "
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.155277066845!2d139.77218531544662!3d35.69779633667034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188ea7088c2431%3A0x88c18d925f59141!2z44OT44O844OG44Kv44OO44Ot44K444O85qCq5byP5Lya56S-!5e0!3m2!1sen!2sjp!4v1625322451746!5m2!1sen!2sjp"
         width="600"
         height="450"
         style="border:0;"
         allowfullscreen
       ></iframe>
-    </div>
-    <div v-else class="text-center">
-    <v-progress-circular
-      indeterminate
-      color="primary"
-    ></v-progress-circular>
     </div>
   </div>
 </template>
@@ -30,14 +17,7 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "Index",
-  layout: "loggedin",
-
-  data() {
-    return {
-      keyword: "Hexabase",
-      mapApiKey: process.env.mapApiKey
-    };
-  }
+  layout: "loggedin"
 });
 </script>
 
